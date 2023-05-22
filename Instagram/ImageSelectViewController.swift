77bf9437d2d3c833195581ef539ce17c9824a823
ko-miyagi <k-miyagi@i-enter.co.jp>
2023-05-22 15:32:35 +0900

    firebaseのインストールまで完了

---
+++ Instagram/ImageSelectViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageSelectViewController.swift
+//  Instagram
+//
+//  Created by 宮城 光太朗 on 2023/05/22.
+//
+
+import UIKit
+
+class ImageSelectViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram/PostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostViewController.swift
+//  Instagram
+//
+//  Created by 宮城 光太朗 on 2023/05/22.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
